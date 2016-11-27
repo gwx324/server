05fb86ea069a32abbe0275234d22e9654ae849f7
Sergei Golubchik <serg@mariadb.org>
2016-11-28 00:12:00 +0100

    Item_func_like: print a not like b instead of !(a like b)

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -5040,7 +5040,7 @@ Item_func_like::add_key_fields(JOIN *join, KEY_FIELD **key_fields,
                                uint *and_level, table_map usable_tables,
                                SARGABLE_PARAM **sargables)
 {
-  if (is_local_field(args[0]) && with_sargable_pattern())
+  if (!negated && is_local_field(args[0]) && with_sargable_pattern())
   {
     /*
       SELECT * FROM t1 WHERE field LIKE const_pattern
